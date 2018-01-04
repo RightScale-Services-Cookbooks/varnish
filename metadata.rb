a52dbfe8782036837abd3f6017c09e1c983d564f
Andrew Bobulsky <andrew.bobulsky@buyerquest.com>
2018-01-04 01:27:13 -0500

    Increment metadata

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Varnish Software"
 maintainer_email  "tfheen@varnish-software.com"
 license           "Apache 2.0"
 description       "Installs and configures varnish"
-version           "0.8.2"
+version           "0.8.3"
 
 %w{ubuntu debian centos redhat}.each do |os|
   supports os
